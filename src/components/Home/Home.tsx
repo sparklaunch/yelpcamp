74b3d0997953e12f2506bca04d1fdea734143c1a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-08-17 10:32:53 +0900

    Add Desktop Home.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,45 @@
+import { Link } from "react-router-dom";
+import { useState, ChangeEvent } from "react";
 import Layout from "../Layout/Layout";
 import "./Home.scss";
+import magnifier from "../../assets/Search.svg";
 
 function Home() {
+    const [search, setSearch] = useState("");
+    const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
     return (
         <div className="Home">
             <Layout>
-                <h1>Haha</h1>
+                <div className="welcome">
+                    <div className="welcome-header">
+                        <h1>Welcome to YelpCamp!</h1>
+                    </div>
+                    <div className="welcome-description">
+                        <p>
+                            View our hand-picked campgrounds from all over the
+                            world, or add your own.
+                        </p>
+                    </div>
+                    <div className="search-form">
+                        <div className="search-input">
+                            <img src={magnifier} alt="Magnifier" />
+                            <input
+                                type="text"
+                                value={search}
+                                onChange={changeHandler}
+                                placeholder="Search for camps"
+                            />
+                        </div>
+                        <button className="search-button">Search</button>
+                    </div>
+                    <div className="add-campground">
+                        <Link to="/campgrounds/new">
+                            Or add your own campground
+                        </Link>
+                    </div>
+                </div>
             </Layout>
         </div>
     );
