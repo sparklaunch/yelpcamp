09c195c756abeaa12306edf96fb2433b4ab82a98
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-08-18 07:59:37 +0900

    Complete Not Found Page.

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,13 @@
+import "./NotFound.scss";
+
 function NotFound() {
     return (
-        <div>
+        <div className="NotFound">
             <h1>404 Not Found</h1>
+            <p>
+                Sorry for the inconvenience. The page doesn't exist. Please go
+                back.
+            </p>
         </div>
     );
 }
