abeb3cef293c0c4e15c7ee09b1087db40564a8f1
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-08-18 13:00:39 +0900

    Finish Form Validation of New Page.

--- src/components/New/New.tsx
+++ src/components/New/New.tsx
@@ -1,17 +1,30 @@
+import { FormEvent, useState } from "react";
 import Layout from "../Layout/Layout";
 import "./New.scss";
 
 function New() {
+    const [name, setName] = useState<string>("");
+    const [price, setPrice] = useState<string>("");
+    const [image, setImage] = useState<string>("");
+    const [detail, setDetail] = useState<string>("");
+    const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
     return (
         <Layout>
             <div className="New">
                 <div className="header">
                     <h1>Add New Campground</h1>
                 </div>
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="field">
                         <label htmlFor="name">Campground Name</label>
                         <input
+                            required
+                            onChange={(event) => {
+                                setName(event.target.value);
+                            }}
+                            value={name}
                             id="name"
                             type="text"
                             placeholder="Seven Sisters Waterfall"
@@ -19,11 +32,26 @@ function New() {
                     </div>
                     <div className="field">
                         <label htmlFor="price">Price</label>
-                        <input id="price" type="text" placeholder="$149" />
+                        <input
+                            required
+                            pattern="^[0-9\.]*$"
+                            title="Only numerics accepted"
+                            id="price"
+                            type="text"
+                            value={price}
+                            onChange={(event) => {
+                                setPrice(event.target.value);
+                            }}
+                            placeholder="$149"
+                        />
                     </div>
                     <div className="field">
                         <label htmlFor="image">Image</label>
                         <input
+                            value={image}
+                            onChange={(event) => {
+                                setImage(event.target.value);
+                            }}
                             id="image"
                             type="text"
                             placeholder="www.thepinoytraveler.com/2018/01/mt-ulap-diy-dayhike.html"
@@ -32,6 +60,10 @@ function New() {
                     <div className="field">
                         <label htmlFor="detail">Description</label>
                         <textarea
+                            value={detail}
+                            onChange={(event) => {
+                                setDetail(event.target.value);
+                            }}
                             id="detail"
                             rows={10}
                             placeholder="Write the description..."
